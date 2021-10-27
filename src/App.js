22c4cd51c1b534df5f1bad58d2b0921083efcf80
Joseph Chretien-Golden <joechre@gmail.com>
2021-10-27 14:07:41 -0500

    imports on appjs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import { Switch, Route, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
 
 const App = () => {
     return (
-        <div>
-            <h1>Cryptoverse</h1>
+        <div className="app">
+             <div className="navbar">
+            
+            </div>
+            <div className="main">
+
+            </div>
         </div>
-    )
+    );
 }
 
 export default App
