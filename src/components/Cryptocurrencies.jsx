c484904f2fd54e435aa1ac465fca4032314efd21
Joseph Chretien-Golden <joechre@gmail.com>
2021-11-01 14:41:12 -0500

    crypto cards show all on cryptos page

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -5,24 +5,28 @@ import { Input, Row, Col, Card } from 'antd';
 
 import { useGetCryptosQuery } from '../services/cryptoApi';
 
-const Cryptocurrencies = () => {
-const { data: cryptosList, isFetching } = useGetCryptosQuery();
+const Cryptocurrencies = ({ simplified }) => {
+    const count = simplified ? 10 : 100;
+const { data: cryptosList, isFetching } = useGetCryptosQuery(count);
 const [cryptos, setCryptos] = useState(cryptosList?.data?.coins);
 
 console.log(cryptos);
+
+if(isFetching) return 'Loading...';
     return (
         <>
             <Row gutter={[32, 32]} className="crypto-card-container">
-                {cryptos.map((currency) => (
+                {cryptos?.map((currency) => (
                     <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
-                        <Link>
-                            <Card title={`${currency.rank}. ${currency.name}`}
+                        <Link to={`/crypto/${currency.id}`}>
+                            <Card 
+                            title={`${currency.rank}. ${currency.name}`}
                             extra={<img className="crypto-image" src={currency.iconUrl} />}
                             hoverable>
 
                                 <p>Price: {millify(currency.price)}</p>
                                 <p>Market Cap: {millify(currency.marketCap)}</p>
-                                <p>Daily Change: {millify(currency.change)}</p>
+                                <p>Daily Change: {millify(currency.change)}%</p>
                             </Card>
                         </Link>
                     </Col>
