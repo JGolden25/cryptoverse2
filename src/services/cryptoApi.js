9456eead86330cafc5c6495644400d535588ed74
Joseph Chretien-Golden <joechre@gmail.com>
2021-10-31 19:43:47 -0500

    configurestore established, api query built

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,34 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query';
+
+const cryptoApiHeaders = {
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+    'x-rapidapi-key': '2a2b9f6944mshbf489b5d1d29da5p18ae92jsn56deaf55fa53'
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com/stats';
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders })
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCrypto: builder.query({
+            query: () => createRequest('/exchanges')
+        })
+    })
+});
+// var options = {
+//     method: 'GET',
+//     url: 'https://coinranking1.p.rapidapi.com/stats',
+//     headers: {
+//       'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+//       'x-rapidapi-key': '2a2b9f6944mshbf489b5d1d29da5p18ae92jsn56deaf55fa53'
+//     }
+//   };
+  
+//   axios.request(options).then(function (response) {
+//       console.log(response.data);
+//   }).catch(function (error) {
+//       console.error(error);
+//   });
