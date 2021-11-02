dc6c6c1d5ae86ddb63e452cd41815b89d8bfe7e7
Joseph Chretien-Golden <joechre@gmail.com>
2021-11-02 09:39:00 -0500

    adds chart working

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -9,12 +9,13 @@ const LineChart = ({ coinHistory, currentPrice, coinName }) => {
   const coinTimestamp = [];
 
   for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
-    coinPrice.push(coinHistory?.data?.history[i].price);
+    coinPrice.push(coinHistory.data.history[i].price);
+    coinTimestamp.push(new Date(coinHistory.data.history[i].timestamp).toLocaleDateString());
   }
 
-  for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
-    coinTimestamp.push(new Date(coinHistory?.data?.history[i].timestamp).toLocaleDateString());
-  }
+//   for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
+//     coinTimestamp.push(new Date(coinHistory?.data?.history[i].timestamp).toLocaleDateString());
+//   }
 
   const data = {
     labels: coinTimestamp,
